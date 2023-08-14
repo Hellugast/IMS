2493f59956920e82028d351f658c012196be3dcb
Batu YURT <asdasd018@hotmail.com>
2023-08-14 23:32:55 +0300

    Adding Inventory

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,15 @@ namespace IMS.CoreBusiness
     public class Inventory
     {
         public int InventoryId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string InventoryName { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue)]
         public int Quantity { get; set; }
+
+        [Range(0, int.MaxValue)]
         public double Price { get; set; }
     }
 }
